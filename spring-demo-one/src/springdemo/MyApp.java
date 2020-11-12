6e6e3d6f129c61c28d271bd3d14ffc1b8398c85b
anurag9455 <anuragsrivastava56789@gmail.com>
2020-11-12 12:27:30 +0530

    Inversion of Control

---
+++ spring-demo-one/src/springdemo/MyApp.java
@@ -0,0 +1,23 @@
+package springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// create the object
+		
+		Coach theCoach = new TrackCoach();
+		
+		//Coach theBaseballCoach = new BaseballCoach();
+		
+		
+		// use the object
+		
+	System.out.println(theCoach.getDailyWorkout());
+	//System.out.println(theBaseballCoach.getDailyWorkout());
+
+	}
+
+}
